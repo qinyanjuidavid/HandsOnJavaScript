8878fb3d9a2cb61170ce502bddd4f0cbecf26cd8
qinyanjuidavid <davidkinyanjui052@gmail.com>
2021-05-16 11:17:07 +0300

    Assigning values

--- JS/main.js
+++ JS/main.js
@@ -1,3 +1,19 @@
 console.log("Hello World");
 console.error("This is an error");
 console.warn("This is a warning");
+
+//Variables
+//Var,let,const
+//Let-->One can reassign values
+//Var is globally scoped
+let age=30;
+age=31;
+console.log(age);
+//const -- It cant be reassigned
+const salary=250000;
+// salary=600000
+console.log(salary);
+
+let score;
+score=10;
+console.log(score);
