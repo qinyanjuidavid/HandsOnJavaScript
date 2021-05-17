f50cb90d83574cb4f695089ea0c4f9af30b6ba42
qinyanjuidavid <davidkinyanjui052@gmail.com>
2021-05-17 21:54:40 +0300

    Arrays

--- JS/stringfy.js
+++ JS/stringfy.js
@@ -18,3 +18,26 @@ console.log(s.substring(0,5).toUpperCase());
 console.log(s.split(''));
 const word='technology, computers, IT, Code';
 console.log(word.split(','));
+//Arrays-->Variables that hold multiple values
+/*Multi
+lined Comments
+*/
+const numbers=new Array(1,2,3,4,5);
+console.log(numbers);
+const fruits=['apple','oranges','pears'];
+console.log(fruits);
+console.log(typeof fruits);
+console.log(fruits[1]);
+console.log(fruits[10]);
+fruits[3]='grapes';
+console.log(fruits);
+fruits.push('Water Melon');
+console.log(fruits);
+fruits.unshift("Strawberries");
+console.log(fruits);
+fruits.push("Pine Apples");
+console.log(fruits);
+fruits.pop();
+console.log(fruits);
+console.log(Array.isArray(fruits));
+console.log(Array.isArray("apple"));
