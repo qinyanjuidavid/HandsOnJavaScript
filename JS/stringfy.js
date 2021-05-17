88922f4124926b00ae01b30f589e946507f5de7a
qinyanjuidavid <davidkinyanjui052@gmail.com>
2021-05-17 21:37:27 +0300

    Initial Commit

---
+++ JS/stringfy.js
@@ -0,0 +1,14 @@
+//String Concatination
+const name="Jane Doe";
+const age=30;
+console.log("My name is "+name+" I am "+age+" years old.");
+//Template String
+const hello="My name is ${name} and I am ${age} years old";
+console.log(hello);
+let salary=256000
+console.log('My salary is ${salary} KSH.');
+//Above metods does not work in my code...
+//Additional methods
+const s="Hello Friend";
+console.log(s.length);
+console.log(s.toUpperCase());
