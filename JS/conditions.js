fc04b9dd388af96d3cead70cf09221ab89e0239e
qinyanjuidavid <davidkinyanjui052@gmail.com>
2021-05-19 22:24:43 +0300

    Working on a simple example

---
+++ JS/conditions.js
@@ -0,0 +1,25 @@
+const x=20;
+if (x===10){
+  console.log("X is 10");
+}
+else if(x>10){
+  console.log("X is greater than 10");
+}
+else{
+  console.log("X is not 10");
+}
+
+let maths=77;
+let sst=75;
+let scie=70;
+let kis=78;
+let eng=65;
+
+let total=maths+sst+scie+kis+eng;
+var avg=total/5;
+if (avg=>80 && avg<=100){
+  console.log("B+");
+}
+else{
+  console.log("This line is always printed");
+}
