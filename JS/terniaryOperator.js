0c072e3ad44008220034310ea8168923431cab46
qinyanjuidavid <davidkinyanjui052@gmail.com>
2021-05-20 22:27:16 +0300

    Worked on the Switch statement

--- JS/terniaryOperator.js
+++ JS/terniaryOperator.js
@@ -20,3 +20,31 @@ switch(c){
     break;
 }
 //Ttial
+
+const dayOfTheWeek=7
+switch(dayOfTheWeek){
+  case 1:
+    console.log("Sunday");
+    break;
+  case 2:
+    console.log("Monday");
+    break;
+  case 3:
+    console.log("Tuesday");
+    break;
+  case 4:
+    console.log("Wednesday");
+    break;
+  case 5:
+    console.log("Thursday");
+    break;
+  case 6:
+    console.log("Friday");
+    break;
+  case 7:
+    console.log("Saturday");
+    break;
+  default:
+    console.log("Invalid Day!");
+    break;
+}
