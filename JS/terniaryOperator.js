da92937966902bedf71f2ada007d9ad7e63e60a0
qinyanjuidavid <davidkinyanjui052@gmail.com>
2021-05-20 22:20:48 +0300

    Working on the switch statements

--- JS/terniaryOperator.js
+++ JS/terniaryOperator.js
@@ -6,3 +6,17 @@ const day=5
 
 const d=1===day?'Sunday':2===day?'Monday':3===day?"Tuesday":day===4?"Wednesday":day===5?"Thursday":day===6?"Friday":day===7?"Sutday":"Invalid Entry";
 console.log(d);
+//Switch
+let c="blue"
+switch(c){
+  case 'red':
+    console.log("Color is red");
+    break;
+  case 'blue':
+    console.log("Color is blue");
+    break;
+  default:
+    console.log("Color is not red or blue");
+    break;
+}
+//Ttial
