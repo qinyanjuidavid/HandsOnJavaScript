01159c61bd5634f6d45ec3fb7e43769cc800642b
qinyanjuidavid <davidkinyanjui052@gmail.com>
2021-05-20 22:16:37 +0300

    Worked with the terniary operators

---
+++ JS/terniaryOperator.js
@@ -0,0 +1,8 @@
+const x=11;
+const color=x>10 ? 'red' :'blue';
+console.log(color);
+
+const day=5
+
+const d=1===day?'Sunday':2===day?'Monday':3===day?"Tuesday":day===4?"Wednesday":day===5?"Thursday":day===6?"Friday":day===7?"Sutday":"Invalid Entry";
+console.log(d);
