7b16c60932205dbcfcdee70b44445c58d6b9b4cf
qinyanjuidavid <davidkinyanjui052@gmail.com>
2021-05-21 21:13:39 +0300

    Hands on basic functions

--- JS/funct.js
+++ JS/funct.js
@@ -1,3 +1,10 @@
-function AddNumbers(){
-  
+function AddNumbers(num1,num2){
+  const sum=num1+num2;
+  console.log(sum);
 }
+AddNumbers(4,5);
+//Setting default values in the arguments
+function SubNumbers(n1=1,n2=2){
+  console.log(n2-n1);
+}
+SubNumbers();
