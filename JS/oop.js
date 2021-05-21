93b9dbb66e329ed4499a02f618e216b5044f2a6e
qinyanjuidavid <davidkinyanjui052@gmail.com>
2021-05-21 21:28:58 +0300

    ..

--- JS/oop.js
+++ JS/oop.js
@@ -1,7 +1,7 @@
 function Person(firstName,lastName,dob){
   this.firstName=firstName;
   this.lastName=lastName;
-  this.dob=dob;
+  this.dob=new Date(dob);
 }
 const p1= new Person("John ","Doe","4-3-1970");
 console.log(p1);
@@ -9,3 +9,4 @@ const p2=new Person("Jane", "Doe","12-12-1970");
 console.log(p2);
 console.log(p1.firstName);
 console.log(p2.firstName);
+console.log(p2.dob);
